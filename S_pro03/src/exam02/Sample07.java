26d9fd237b294874945bfb49845c9e66b35c1356
최신형 <tlsgudrhfem@naver.com>
2022-03-11 09:43:11 +0900

    중첩 반복문 / 구구단

---
+++ S_pro03/src/exam02/Sample07.java
@@ -0,0 +1,31 @@
+package exam02;
+
+public class Sample07 {
+
+	public static void main(String[] args) {
+		/* 
+		 * 중첩반복문
+		 */
+		
+		for(int i = 1; i <= 3; i++) {
+			System.out.println("i가 1번 반복할 때마다");
+			for(int j = 1; j <= 3; j++) {
+				System.out.println("j의; 반복은 3번씩 총 9번 이루어진다.");
+			}
+		}
+		
+		/* 
+		 * 구구단
+		 */
+		
+		for(int i = 2; i <= 9; i++) {
+			for(int j = 1; j <= 9; j++) {
+				System.out.printf("%d x %d = %d\n", i, j, i * j);
+			}
+			System.out.println();
+		}
+		
+
+	}
+
+}
