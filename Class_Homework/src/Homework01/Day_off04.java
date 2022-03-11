d8312412c1ab702ef545e0582c04c43ab282832c
최신형 <tlsgudrhfem@naver.com>
2022-03-11 09:19:26 +0900

    사용자입력 구구단

--- Class_Homework/src/Homework01/Day_off04.java
+++ Class_Homework/src/Homework01/Day_off04.java
@@ -8,15 +8,14 @@ public class Day_off04 {
 		/* 
 		 * 사용자가 입력한 정수값에 해당하는 구구단을 출력하시오
 		 */
-		
-		int num, i;
-		
-		System.out.print("원하는 단의 숫자를 입력해주세요 -> ");
+	
 		Scanner sc = new Scanner(System.in);
+		System.out.print("원하는 단의 숫자를 입력해주세요 -> ");
+
 		
-		num = sc.nextInt();
+		int num = sc.nextInt();
 		
-		for(i = 1; i <= 9; i++) {
+		for(int i = 1; i <= 9; i++) {
 			System.out.printf("%d X %d = %d\n", num, i, num * i);
 		}
 		
