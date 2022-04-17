19161b1c4833a1cd1fa6330cd0e772ff8c2df877
최신형 <tlsgudrhfem@naver.com>
2022-04-17 23:43:28 +0900

    예외처리

---
+++ S_pro09/src/exception/GradeException.java
@@ -0,0 +1,15 @@
+package exception;
+
+public class GradeException extends Exception {
+	
+	public GradeException() {
+		super();
+
+	}
+
+	public GradeException(String message) {
+		super(message);
+
+	}
+
+}
---
+++ S_pro10/src/exam01/UserDefindException.java
@@ -0,0 +1,13 @@
+package exam01;
+
+public class UserDefindException extends Exception {
+	
+	public UserDefindException() {
+	
+	}
+	
+	public UserDefindException(String message) {
+		super(message);
+	}
+
+}
